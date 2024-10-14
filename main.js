e12f25d1d55d273c28ed70c223574ce12dbe0748
Danilo $antos <daniloribeiro761@gmail.com>
2024-10-14 14:21:50 -0300

    commit4

---
+++ main.js
@@ -0,0 +1,25 @@
+let wins = 150;
+let lose = 5;
+let balanceWins = wins - lose;
+
+
+
+    function calculo(){ 
+    if (balanceWins < 10) {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Ferro`);
+    } else if (balanceWins >= 11 && balanceWins < 20) {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Bronze`);
+    } else if (balanceWins >= 21 && balanceWins < 50) {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Prata`);
+    } else if (balanceWins >= 51 && balanceWins < 80) {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Ouro`);
+    } else if (balanceWins >= 81 && balanceWins < 90) {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Diamante`);
+    } else if (balanceWins >= 91 && balanceWins < 100) {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Lendário`);
+    } else {
+      console.log(`O herói tem saldo de ${balanceWins} e está no nível Imortal`);
+    }
+    }
+    calculo()
+  
